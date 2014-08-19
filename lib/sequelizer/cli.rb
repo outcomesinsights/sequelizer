5de1ae3e41956279c2e3d920410b54bb4cebcd1b
Ryan Duryea <aguynamedryan@gmail.com>
2014-08-18 20:13:32 -0700

    bin/sequelizer: add init_env function

--- lib/sequelizer/cli.rb
+++ lib/sequelizer/cli.rb
@@ -11,9 +11,53 @@ module Sequelizer
       GemfileModifier.new(options).modify
     end
 
+    desc 'init_env', 'creates a .env file with the parameters listed'
+    option :adapter,
+      aliases: :a,
+      desc: 'adapter for database'
+    option :host,
+      aliases: :h,
+      banner: 'localhost',
+      desc: 'host for database'
+    option :username,
+      aliases: :u,
+      desc: 'username for database'
+    option :password,
+      aliases: :P,
+      desc: 'password for database'
+    option :port,
+      aliases: :p,
+      type: :numeric,
+      banner: '5432',
+      desc: 'port for database'
+    option :database,
+      aliases: :d,
+      desc: 'database for database'
+    option :search_path,
+      aliases: :s,
+      desc: 'schema for database (PostgreSQL only)'
+    def init_env
+      if File.exist?('.env')
+        puts ".env already exists!  I'm too cowardly to overwrite it!"
+        puts "Here's what I would have put in there:"
+        puts make_env(options)
+        exit(1)
+      end
+      File.open('.env', 'w') do |file|
+        file.puts make_env(options)
+      end
+    end
+
     desc 'config', 'prints out the connection parameters'
     def config
       pp Options.new.to_hash
+    end
+
+    private
+    def make_env(options)
+      options.map do |key, value|
+        "SEQUELIZER_#{key.upcase}=#{value}"
+      end.join("\n")
     end
   end
 end
