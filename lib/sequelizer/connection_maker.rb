80ba626aa2a298793a08c650161d7e5811c468cf
Jeremy Evans <code@jeremyevans.net>
2017-11-30 21:26:41 -0800

    Add connection abstraction for Impala and PostgreSQL
    This tries to abstract access parameters across the following
    adapters (with recognized :adapter values in parentheses):
    
    * jdbc/impala (:jdbc_impala)
    * jdbc/hive2 (:jdbc_hive2)
    * jdbc/postgresql (:jdbc_postgres)
    * impala (:impala)
    * postgres (:postgres)
    
    It takes the following general connection options:
    
    * :user
    * :password
    * :host
    * :port
    * :database
    
    And for impala the following Kerberos related options:
    
    * :realm
    * :host_fqdn
    * :principal
    
    For the jdbc adapters it tries to construct the appropriate
    JDBC URL, and for the impala adapter, it tries to construct the
    correct connection hash.
    
    This has had basic (non-Kerberos) testing so far on all adapters.
    Specifically, the jdbc/hive2, jdbc/impala, and impala adapters
    all work with the only change being the :adapter option.
    
    This actually doesn't have any code specific to the :postgres
    adapter, since the standard options work there.

--- lib/sequelizer/connection_maker.rb
+++ lib/sequelizer/connection_maker.rb
@@ -1,4 +1,5 @@
 require 'sequel'
+require 'cgi'
 require_relative 'options'
 
 module Sequelizer
@@ -24,13 +25,72 @@ module Sequelizer
     def connection
       opts = options.to_hash
       extensions = options.extensions
+
       conn = if url = (opts.delete(:uri) || opts.delete(:url))
-               Sequel.connect(url, opts)
-             else
-               Sequel.connect(opts)
-             end
+        Sequel.connect(url, opts)
+      else
+        # Kerberos related options
+        realm = opts[:realm]
+        host_fqdn = opts[:host_fqdn] || opts[:host]
+        principal = opts[:principal]
+
+        adapter = opts[:adapter]
+        if adapter =~ /\Ajdbc_/
+          user = opts[:user]
+          password = opts[:password]
+        end
+
+        case opts[:adapter]
+        when :jdbc_hive2
+          opts[:adapter]  = :jdbc
+          auth = if realm
+            ";principal=#{e principal}/#{e host_fqdn}@#{e realm}"
+          elsif user
+            ";user=#{e user};password=#{e password}"
+          else
+            ';auth=noSasl'
+          end
+          opts[:uri] = "jdbc:hive2://#{e opts[:host]}:#{opts.fetch(:port, 21050).to_i}/#{e(opts[:database] || 'default')}#{auth}"
+        when :jdbc_impala
+          opts[:adapter]  = :jdbc
+          auth = if realm
+            ";AuthMech=1;KrbServiceName=#{e principal};KrbAuthType=2;KrbHostFQDN=#{e host_fqdn};KrbRealm=#{e realm}"
+          elsif user
+            if password
+              ";AuthMech=3;UID=#{e user};PWD=#{e password}"
+            else
+              ";AuthMech=2;UID=#{e user}"
+            end
+          end
+          opts[:uri] = "jdbc:impala://#{e opts[:host]}:#{opts.fetch(:port, 21050).to_i}/#{e(opts[:database] || 'default')}#{auth}"
+        when :jdbc_postgres
+          opts[:adapter]  = :jdbc
+          auth = "?user=#{user}#{"&password=#{password}" if password}" if user
+          opts[:uri] = "jdbc:postgresql://#{e opts[:host]}:#{opts.fetch(:port, 5432).to_i}/#{e(opts[:database])}#{auth}"
+        when :impala
+          opts[:database] ||= 'default'
+          opts[:port] ||= 21000
+          if principal
+            # realm doesn't seem to be used?
+            opts[:transport] = :sasl
+            opts[:sasl_params] = {
+              mechanism: "GSSAPI",
+              remote_host: host_fqdn,
+              remote_principal: principal
+            }
+          end
+        end
+
+        Sequel.connect(opts)
+      end
       conn.extension(*extensions)
       conn
+    end
+
+    private
+
+    def e(v)
+      CGI.escape(v.to_s)
     end
   end
 end
