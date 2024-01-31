d91c98ba81a6e642dfad5ed091bf8517167f4bef
Ryan Duryea <aguynamedryan@gmail.com>
2024-01-31 13:41:36 -0800

    Rename test for Usable

--- test/lib/sequel/extensions/test_usable.rb
+++ test/lib/sequel/extensions/test_usable.rb
@@ -3,11 +3,10 @@ require 'sequel'
 require 'sequel/extensions/usable'
 
 class TestUsable < Minitest::Test
-  def test_should_detect_options_for_appropriate_db
+  def test_should_call_use
     db = Sequel.mock(host: :sqlite)
     db.extension :usable
     db.use(:some_schema)
     assert_equal(db.sqls, ["USE `some_schema`"])
   end
 end
-
